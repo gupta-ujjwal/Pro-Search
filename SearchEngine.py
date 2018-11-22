82a463993f1b06d630a79afa931fd063df300207
gupta-ujjwal <gupta.ujjwal14@gmail.com>
2018-11-23 02:42:33 +0530

    Added cosine similarity

--- SearchEngine.py
+++ SearchEngine.py
@@ -1,6 +1,10 @@
 import csv
+import math
+
 matrix = []
 doc = []
+ranking = []
+links = []
 
 def filterToken(tokens):
     for a in tokens:
@@ -18,14 +22,19 @@ def readFile():
             matrix.append(row)
     csvFile.close()
 
+def column(mat, i):
+    return [row[i] for row in mat]
+
 def retrieveDocs():
     global doc,matrix,vis_links,query
+    doc.append(column(matrix, 0))
     for k in query:
         for i in range(n):
             if(matrix[i][0] == k):
                 for j in range(1,m):
-                    if (float(matrix[i][j])>0.8) and vis_links[j] not in doc:
-                        doc.append(vis_links[j])
+                    if (float(matrix[i][j])>0.8) and vis_links[j] not in links:
+                        doc.append(column(matrix, j))
+                        links.append(vis_links[j])
 
 readFile()
 
@@ -39,4 +48,33 @@ m = len(matrix[0])-1
 query = filterToken(input("Enter Query - ").lower().split(' '))
 
 retrieveDocs()
+
+for i in range(len(links)):
+    ranking.append(0)
+   
+queryArray = []
+queryAmp = 0
+for i in doc[0]:
+    if i in query:
+        queryArray.append(1)
+        queryAmp = queryAmp + 1
+    else:
+        queryArray.append(0)
+
+queryAmp = math.sqrt(queryAmp)
+
+for i in range(1,len(doc)):
+    sum = 0
+    amp = 0
+    for j in range(len(doc[i])):
+        amp = amp + float(doc[i][j])
+        if doc[0][j] in query:
+            sum = sum + float(doc[i][j])
+    amp = math.sqrt(amp)
+    ranking[i-1] = sum/(amp*queryAmp)
+
+print(max(ranking))
+            
+
+
 
